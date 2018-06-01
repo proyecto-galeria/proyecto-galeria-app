878856154c5cc166a9295c670b156cf96830331f
Leopoldo Caballero <pitakill@descartes.local>
2018-05-31 21:39:22 -0500

    Menu via Dropdown

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,33 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {
+    DropdownButton,
+    MenuItem,
+} from 'react-bootstrap';
 
 const Menu = () => (
-<ul>
-    <li>
-        <Link to="/">Inicio</Link>
-    </li>
-    <li>
-        <Link to="/concept">Concepto</Link>
-    </li>
-    <li>
-        <Link to="/record">Registro</Link>
-    </li>
-    <li>
-        <Link to="/photo">Foto</Link>
-    </li>
-    <li>
-        <Link to="/validate-photo">Validar Foto</Link>
-    </li>
-    <li>
-        <Link to="/validate-record">Validar Registro</Link>
-    </li>
-    <li>
-        <Link to="/add-receiver">Agregar destinatario</Link>
-    </li>
-    <li>
-        <Link to="/feedback">Retroalimentación</Link>
-    </li>
-</ul>
+<DropdownButton
+    title="Menú"
+    id="Menú"
+    >
+    <MenuItem><Link to="/">Inicio</Link></MenuItem>
+    <MenuItem><Link to="/concept">Concepto</Link></MenuItem>
+    <MenuItem><Link to="/record">Registro</Link></MenuItem>
+    <MenuItem><Link to="/photo">Foto</Link></MenuItem>
+    <MenuItem><Link to="/validate-photo">Validar Foto</Link></MenuItem>
+    <MenuItem><Link to="/validate-record">Validar Registro</Link></MenuItem>
+    <MenuItem><Link to="/add-receiver">Agregar destinatario</Link></MenuItem>
+    <MenuItem><Link to="/feedback">Retroalimentación</Link></MenuItem>
+</DropdownButton>
 );
 
 export default Menu;
