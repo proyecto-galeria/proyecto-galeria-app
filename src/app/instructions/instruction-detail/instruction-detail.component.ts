d011356da86456a39b871cb00a0c45211378c548
furenku <furenku@gmail.com>
2018-08-02 21:05:32 -0500

    fetch concept in instruction detail, add markup

--- src/app/instructions/instruction-detail/instruction-detail.component.ts
+++ src/app/instructions/instruction-detail/instruction-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Instruction } from '../../shared/models/instruction.model';
+import { ConceptsService } from '../../concepts/concepts.service';
 
 @Component({
   selector: 'instruction-detail',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InstructionDetailComponent implements OnInit {
 
-  constructor() { }
+  public instruction: Instruction;
+
+  constructor(
+    private route: ActivatedRoute,
+    private conceptService: ConceptsService,
+  ) { }
 
   ngOnInit() {
+
+    this.instruction = this.route.snapshot.data['instruction'] || {};
+
+    this.conceptService.fetchConcept(this.instruction.concept)
+    .subscribe( concept => this.instruction.concept_object = concept );
+
   }
 
 }
