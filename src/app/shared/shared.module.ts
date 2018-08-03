fc99b5699893f7716b6f48aa5f13b4a7bf1006a4
furenku <furenku@gmail.com>
2018-08-02 21:29:54 -0500

    added angular google maps

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AgmCoreModule } from "@agm/core";
+
 import { SharedRoutingModule } from './shared-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCEqQWUTDJJS6uWJXAdWQRA381hGoI1oHk'
+    }),
     SharedRoutingModule
   ],
   declarations: []
