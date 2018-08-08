06f229aa4dd92b7faedd999bd4c5ff37040c4296
furenku <furenku@gmail.com>
2018-08-07 20:32:04 -0500

    instruction list

--- src/app/instructions/instruction-list/instruction-list.component.ts
+++ src/app/instructions/instruction-list/instruction-list.component.ts
@@ -6,6 +6,8 @@ import { ActivatedRoute } from "@angular/router";
 
 import { InstructionsService } from "../instructions.service";
 import { Instruction } from '../../shared/models/instruction.model';
+import { ConceptsService } from '../../concepts/concepts.service';
+import { LocationsService } from '../../locations/locations.service';
 
 
 @Component({
@@ -17,12 +19,28 @@ export class InstructionListComponent implements OnInit {
 
   public instructions: Instruction[];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private conceptsService: ConceptsService,
+    private locationsService: LocationsService
+  ) { }
 
   ngOnInit() {
 
     // obtener listado de instructions
     this.instructions = this.route.snapshot.data['instructions'] || {};
+
+    this.instructions.map( instruction => {
+      this.conceptsService.fetchConcept(instruction.concept)
+      .subscribe( concept => {
+        this.locationsService.fetchLocation(concept.location).subscribe(
+          location => {
+            concept.location_object = location;
+            instruction.concept_object = concept 
+          }
+        )
+      });
+    })
     
   }
   
