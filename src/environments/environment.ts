e14983560eeda446671e7d66c94b406663fd10db
furenku <furenku@gmail.com>
2018-08-07 21:08:55 -0500

    added cloudinary keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,15 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { cloudinaryKeys } from "./cloudinary-keys";
+
 export const environment = {
-  production: false
+  production: false,
+
+  cloudinary: {
+    api_key: cloudinaryKeys.api_key,
+    api_secret: cloudinaryKeys.api_secret
+  }
 };
 
 /*
