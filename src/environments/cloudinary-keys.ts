86462ac0600848feaaa6e7afbc11b431f09c7724
furenku <furenku@gmail.com>
2018-08-07 21:14:05 -0500

    removed true keys

--- src/environments/cloudinary-keys.ts
+++ src/environments/cloudinary-keys.ts
@@ -1,4 +1,4 @@
 export let cloudinaryKeys = {
-    api_key: '337927941361121',
-    api_secret: 'FhVicko2_YRo4fNDVt1JQ6ve-9Q'
+    api_key: '***************',
+    api_secret: '***************************'
 }
